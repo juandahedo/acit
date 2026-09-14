a685b883024c3da6be151d1545638d2cef4b5778
Juan David <juandavid@gmail.com>
2026-09-14 16:27:46 -0700

    Add lab01.py

---
+++ lab01.py
@@ -0,0 +1,9 @@
+# What is your full name
+
+print("Juan Hernandez")
+# What is the area of a rectangle with a length of 10 and width of 7?
+print(10 * 7)
+# Fix the line below. Interpreters scan your entire file ahead-of-time to check for errors, unlike compilers.
+print(True)
+# Add one more color to the end of the list, after blue. (Don't forget the comma , and quotes "")
+print(["red", "orange", "blue", "green"])
