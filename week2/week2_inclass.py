72532ac0deb9473fa357c01785713d89de11c22c
Juan David <juandavid@gmail.com>
2026-09-17 16:09:16 -0700

    Add Week 2 in-class activity

---
+++ week2/week2_inclass.py
@@ -0,0 +1,16 @@
+x = 10
+y = "Hello + " " world"
+z = 50.3
+x = "Now I am a string"
+y = "Earth"
+#print (bool(y))
+z = 63.4
+y = "New string"
+
+print(x)
+print(type(x))
+course_code = "CIT"
+course_number = "1515"
+print("A" + course_code + course_number)
+assignment_number =2
+print(f'Assignment {assignment_number} created')
